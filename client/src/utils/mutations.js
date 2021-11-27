3e5a7d01b40cb9777c1509ba7cab278307d4d432
CristobalHdz <cristobalhdzch@gmail.com>
2021-11-27 11:58:33 -0700

    Fix login and sign up

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,7 @@
-import gql from "graphical-tag"
+
+   
+import gql from "graphql-tag";
+
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag"
 
-export const getMe = gql`
+export const GET_ME = gql`
 {
     me{
         username
